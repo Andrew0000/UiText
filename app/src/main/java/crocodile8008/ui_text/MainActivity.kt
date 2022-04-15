d2b0690068a37f4b4224be27c5ec928a3f948d3d
crocodile8 <crocodile8000@gmail.com>
2022-04-16 00:07:18 +0300

    Initial

---
+++ app/src/main/java/crocodile8008/ui_text/MainActivity.kt
@@ -0,0 +1,12 @@
+package crocodile8008.ui_text
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
